95c152e8091889623c90aaa6401b700d102d4557
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-11-25 22:34:59 +0200

    Creating needed components

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector : 'app-recipe-detail',
+    templateUrl : './recipe-detail.component.html',
+    styleUrls : ['./recipe-detail.componet.css']
+})
+export class RecipeDetailComponent implements OnInit
+{
+    constructor()
+    {
+
+    }
+
+    ngOnInit()
+    {
+        
+    }
+}
