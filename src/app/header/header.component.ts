3f141c28e743f6a86beac0f78cc00eb3972c3764
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2018-12-01 17:56:33 +0200

    add Navigation with Event binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  @Output() featureSelected = new EventEmitter<string>();
 
   ngOnInit() {
+  }
+  
+  onRecipesSelecet(feature: string) {
+    this.featureSelected.emit(feature);
+
+  }
+  onShoppingListSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 
 }
